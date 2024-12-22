dbb85c680a8684f6accd5a758a88de1d19204fcc
DushyanthaAT <dushyantha721@gmail.com>
2024-12-22 20:02:12 +0530

    complete home screen top

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import NenasaLogo from "../assets/NenasaLogo.png";
-import MenuIcon from "@mui/icons-material/Menu";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Items = [
   {
@@ -25,22 +26,59 @@ const Items = [
   },
 ];
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex p-5">
+    <div className="flex pt-5 px-7 items-center justify-between">
       <img
         src={NenasaLogo}
         alt="Nenasa Logo"
-        className="w-20  h-auto object-contain"
+        className="w-24 h-auto object-contain"
       />
-      <ul className=" gap-3 hidden">
+      <ul className="gap-10 hidden sm:flex">
         {Items.map((Item) => (
-          <li key={Item.id}>
+          <li
+            key={Item.id}
+            className=" text-gray-500 font-semibold text-lg hover:text-pri_blue"
+          >
             <a href={Item.link}>{Item.title}</a>
           </li>
         ))}
       </ul>
 
-      <MenuIcon className="text:red" />
+      <div className="sm:hidden cursor-pointer" onClick={() => setOpen(!open)}>
+        <div className="transition-transform duration-500 ease-in-out">
+          {open ? (
+            <HiMenuAlt2
+              style={{ stroke: "#1B609A", strokeWidth: "1" }}
+              className="text-3xl text-pri_blue"
+            />
+          ) : (
+            <HiMenuAlt3
+              style={{ stroke: "#1B609A", strokeWidth: "1" }}
+              className="text-3xl text-pri_blue"
+            />
+          )}
+        </div>
+      </div>
+
+      {/* responsiveMenu */}
+      <div
+        className={`absolute sm:hidden top-16 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform duration-300 ${
+          open ? "opacity-100" : "opacity-0"
+        }`}
+        style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+      >
+        <ul className="flex flex-col gap-3 w-full items-center py-2">
+          {Items.map((Item, index) => (
+            <li
+              key={Item.id}
+              className="text-gray-500 hover:text-white hover:bg-pri_blue w-[90%] text-center py-2 rounded-lg"
+            >
+              <a href={Item.link}>{Item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import homeImage from "../assets/homeImage.png";
+import nenasaLogo from "../assets/nenasaLogo.png";
 
 const Home = () => {
   return (
-    <>
+    <div className="mx-0 md:mx-16 lg:mx-32">
       <Navbar />
-    </>
+
+      <div className="flex flex-col items-center sm:flex-row sm:pt-10">
+        <div className="flex-1 flex flex-col relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto aspect-square items-center justify-center sm:justify-normal">
+          <img src={homeImage} alt="student" className="w-[80%]" />
+          <div className="w-32 h-32 bg-pri_blue absolute bottom-10 right-10 opacity-90 rounded-xl flex flex-col justify-center items-center text-white ">
+            <span className="text-4xl font-semibold">3</span>
+            <span className="text-center">Projects Completed</span>
+          </div>
+        </div>
+        <div className="flex flex-col px-4 flex-1 gap-4">
+          <img src={nenasaLogo} alt="nenasa logo" className="w-40" />
+          <span className="text-justify text-lg md:text-xl lg:text-2xl">
+            IEEE නැණස, is collaborative project between the SLSAC and IEEE Sri
+            Lanka Section SIGHT with the intention of promoting STEM education
+            among school students in the country. School students from mainly
+            underprivileged communities are encouraged to pursue a journey in
+            STEM education going beyond the school curriculum.
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
