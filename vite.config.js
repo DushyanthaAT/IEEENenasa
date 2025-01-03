43517ac0ccb988f9f04867bf36335c1884cd2c44
DushyanthaAT <dushyantha721@gmail.com>
2025-01-03 09:37:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://ieee-nenasa-backend.vercel.ap",
+        target: "https://ieee-nenasa-backend.vercel.app",
         secure: false,
       },
     },
